751510295117b48fbb639b7852e0e61afdd97068
Ji Kim <jiyoung.kimmy@gmail.com>
2023-03-08 00:57:02 -0500

    strings

---
+++ strings.js
@@ -0,0 +1,7 @@
+// Writing out JS String Content
+
+// ---- tempplate literals ---
+let item = 'cucumbers';
+let price = '$46.00';
+
+console.log(`I would like you to purchase a ${item} that is ${price}`);

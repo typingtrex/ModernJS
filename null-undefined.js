011f6209dde64b2480a559cc7b7610e6e01670a2
Ji Kim <jiyoung.kimmy@gmail.com>
2023-03-12 22:17:13 -0400

    null and undefined

---
+++ null-undefined.js
@@ -0,0 +1,10 @@
+// ---- JS -- let's break down null and undefined ----
+/*
+null is used if there is nothing right now
+ie. user is null until someone logs in
+null is when a variable is INTENTIONALLY not defined
+
+nothing is defined -- undefined
+variables that are not assigned a value
+
+*/
--- strings.js
+++ strings.js
@@ -10,3 +10,4 @@ console.log(`I would like you to purchase a ${item} that are $${price + 10.5}`);
 
 console.log(`I want ${quantity} ${item}`);
 console.log(`I am ${age} years old.`);
+console.log(`You bought ${quantity} ${item}. And it cost ${quantity * price}.`);

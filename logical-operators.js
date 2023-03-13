3dc2fc56fab548e3c03b833fd0ec0657186e6294
Ji Kim <jiyoung.kimmy@gmail.com>
2023-03-12 23:54:27 -0400

    truthy falsy and logical operators

---
+++ logical-operators.js
@@ -0,0 +1,28 @@
+// ALL THE LOGICAL OPERATORS
+/*
+PRECEDENCE
+! then AND then ||
+*/
+
+// ---- AND && -------------------
+/*
+Both A && B need to be true for the statement to be true
+FALSE IF:
+1. A is true && B is false
+2. A is false && B is false
+3. A is true && B is false
+*/
+
+// ---- OR || --------------------
+/*
+Both A && B need to be false for the statement to be false
+TRUE IF:
+1. A is true && B is false
+2. A is false && B is false
+3. A is true && B is false
+*/
+
+// ---- NOT ! --------------------
+/*
+using !== or ! in front of a variable
+*/
---
+++ truthy-vs-falsy.js
@@ -0,0 +1,13 @@
+// truthy and falsy values
+
+// all values have an inherent truthy or falsy boolean value
+// everything is truthy except the following falsy:
+/*
+falsy values:
+1. false
+2. 0
+3. "" (empty string)
+4. null
+5. undefined
+6. NaN (not a number)
+*/

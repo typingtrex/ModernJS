1b3c648add3dc5416eb9e2c6dde33f023afce35b
Ji Kim <jiyoung.kimmy@gmail.com>
2023-03-23 16:05:46 -0400

    arrays

---
+++ app.js
@@ -0,0 +1,2 @@
+// just to make an app.js file
+alert("it's working!");
---
+++ arrays.js
@@ -0,0 +1,31 @@
+// ---- CREATING ARRAYS ----------
+// array literals
+const arrayLiterals = [];
+
+// ---- ARRAY INDICES ----------
+// js arrays are indexed at 0
+// js array's lengths are array.length
+console.log(arrayLiterals.length);
+
+// ---- MODIFYING ARRAYS ----------
+const colors = ['blue', 'yellow', 'magenta', 'orange'];
+colors[0] = 'greenBLUE';
+console.log(colors);
+
+colors[5] = 'hello'; // cannot add this way
+console.log(colors);
+
+// ---- ARRAY METHODS ----------
+// MDN has a LOT
+// PUSH METHOD -- adds to end of array
+// POP METHOD -- removes at the end of the array
+// SHIFT METHOD -- removes from the front of the array
+// UNSHIFT METHOD -- adds to the front of the array
+
+// ---- CREATING ARRAYS ----------
+
+// ---- CREATING ARRAYS ----------
+
+// ---- CREATING ARRAYS ----------
+
+// ---- CREATING ARRAYS ----------

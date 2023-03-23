01f53fd6ec72b2b42111ef98b7bba21b0c3dea47
Ji Kim <jiyoung.kimmy@gmail.com>
2023-03-23 16:34:25 -0400

    more methods array)

--- arrays.js
+++ arrays.js
@@ -21,11 +21,41 @@ console.log(colors);
 // POP METHOD -- removes at the end of the array
 // SHIFT METHOD -- removes from the front of the array
 // UNSHIFT METHOD -- adds to the front of the array
+// unshifting all together is a different order from unshifting 1 at a time
 
-// ---- CREATING ARRAYS ----------
+// ---- ARRAY CONCAT -- MERGES ARRAYS ----------
+let fruits = ['apple', 'banana'];
+let veggies = ['asparagus', 'brussels sprouts'];
 
-// ---- CREATING ARRAYS ----------
+console.log(fruits.concat(veggies));
+console.log(veggies.concat(fruits));
 
-// ---- CREATING ARRAYS ----------
+// ---- ARRAY INCLUDES RETURNS TRUE----------
+// array.includes("fish")
+// returns true if that exact string is included
 
-// ---- CREATING ARRAYS ----------
+// ---- ARRAY INDEXOF ----------
+// Searches for a value, and if it does exist RETURNS index
+// if it doesn't exist, it'll return -1
+console.log(
+  'this would be to check after index 2, and will return -1',
+  fruits.indexOf('banana', 2)
+);
+
+console.log(
+  'this would be to check after index 0, and will return 1',
+  fruits.indexOf('banana', 0)
+);
+
+// ---- ARRAY JOIN ----------
+// joins the array with whatever is inside join()
+console.log(fruits.join(' -> '));
+
+// ---- ARRAY REVERSE ----------
+console.log(fruits.reverse());
+//reverses order inside the array - actually changes the given array
+console.log(fruits);
+
+// ---- ARRAY SLICE ----------
+// ---- ARRAY SPLICE ----------
+// ---- ARRAY SORT ----------

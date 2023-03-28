697791904728afaf16c3811af712fd5bd54e63a2
Ji Kim <jiyoung.kimmy@gmail.com>
2023-03-28 13:09:20 -0400

    function challenge 2 average complete

--- funcChallenge2-Average.js
+++ funcChallenge2-Average.js
@@ -1,12 +1,14 @@
 // Write a function to find the average value in an array of numbers
-//avg([75,76,80,95,100]) //85.2
 
 const avg = (arr) => {
   let sum = 0;
-  for (const num in arr) {
-    // sum += num;
-    console.log('num is: ', num);
+  for (const num of arr) {
+    sum += num;
+    // console.log('num is: ', num);
   }
+  // console.log('sum is: ', sum);
+  return sum / arr.length;
 };
 
 console.log('average is: ', avg([0, 50])); //25
+console.log('output should be 85.2: ', avg([75, 76, 80, 95, 100])); //85.2

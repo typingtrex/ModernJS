6bb36a4724382038a03d6024e42b6ce0aa02e354
Ji Kim <jiyoung.kimmy@gmail.com>
2023-03-27 22:20:53 -0400

    trying out for in loop)

--- funcChallenge2-Average.js
+++ funcChallenge2-Average.js
@@ -1,3 +1,12 @@
 // Write a function to find the average value in an array of numbers
-//avg([0,50]) //25
 //avg([75,76,80,95,100]) //85.2
+
+const avg = (arr) => {
+  let sum = 0;
+  for (const num in arr) {
+    // sum += num;
+    console.log('num is: ', num);
+  }
+};
+
+console.log('average is: ', avg([0, 50])); //25

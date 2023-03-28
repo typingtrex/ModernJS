2271331c08ad7a8e7fe6be11965db51d36e02d20
Ji Kim <jiyoung.kimmy@gmail.com>
2023-03-28 18:07:54 -0400

    func challenge 4 getCard complete

--- funcChallenge4-getCard.js
+++ funcChallenge4-getCard.js
@@ -8,3 +8,38 @@
 //Pick a random suit from:
 //----clubs,spades, hearts, diamonds
 //Return both in an object
+
+function getCard() {
+  let result = {
+    value: null,
+    suit: null,
+  };
+  const randomValue = () => {
+    const values = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'J',
+      'Q',
+      'K',
+      'A',
+    ];
+    result.value = values[Math.floor(Math.random() * values.length)];
+  };
+  const randomSuit = () => {
+    const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+    result.suit = suits[Math.floor(Math.random() * suits.length)];
+  };
+  randomValue();
+  randomSuit();
+  return result;
+}
+
+console.log('result from getCard function: ', getCard());

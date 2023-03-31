d4fe7f275a229b9739e62920b04e32c655032a1b
Ji Kim <jiyoung.kimmy@gmail.com>
2023-03-30 21:22:38 -0400

    more funcs with func args

--- functions.js
+++ functions.js
@@ -54,3 +54,45 @@ function cryAndComplain() {
 }
 
 runThreeTimes(cryAndComplain);
+
+// ------ udemy's examples of function with a function argument ----------
+/*
+
+// This function accepts another function as an argument
+function callThreeTimes(f) {
+  //And calls it 3 times:
+  f();
+  f();
+  f();
+}
+
+function cry() {
+  console.log("BOO HOO I'M SO SAD!");
+}
+
+function rage() {
+  console.log("I HATE EVERYTHING!");
+}
+
+function repeatNTimes(action, num) {
+  // call action (a function) num number of times
+  for (let i = 0; i < num; i++) {
+    action();
+  }
+}
+
+repeatNTimes(rage, 13);
+
+// Accepts 2 functions as arguments
+// Randomly selects 1 to execute
+function pickOne(f1, f2) {
+  let rand = Math.random();
+  if (rand < 0.5) {
+    f1();
+  } else {
+    f2();
+  }
+}
+
+
+*/
